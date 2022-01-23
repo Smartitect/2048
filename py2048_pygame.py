06c7d0ac49cd90665faa638cdb45035bfb88f13f
Barry Smart <barry.smart@endjin.com>
2022-01-23 15:25:21 +0000

    Got tiles responding to key inputs.

--- py2048_pygame.py
+++ py2048_pygame.py
@@ -27,9 +27,6 @@ CELL_STYLES = {
     4: {"font": TEXT_LIGHT, "fill": pygame.Color(245, 149, 99)}
 }
 
-# Initialize pygame
-pygame.init()
-
 # Define constants for the screen width and height
 BORDER_WIDTH = 10
 TILE_SIZE = 100
@@ -37,21 +34,14 @@ NUMBER_OF_ROWS = NUMBER_OF_COLUMNS = 4
 SCREEN_WIDTH = SCREEN_HEIGHT = ((NUMBER_OF_ROWS + 1) * BORDER_WIDTH) + (NUMBER_OF_ROWS * TILE_SIZE)
 
 FONT_SIZE = 24
-FONT = pygame.font.Font(pygame.font.get_default_font(), FONT_SIZE)
 
-# Create the screen object
-# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen.fill(BACKGROUND)
-
-# Variable to keep the main loop running
-running = True
 
 
 class Tile(pygame.sprite.Sprite):
 
     def __init__(self, row, column, value=0):
         super(Tile, self).__init__()
+        self.font = pygame.font.Font(pygame.font.get_default_font(), FONT_SIZE)
         self.x_pos = BORDER_WIDTH + (row * (BORDER_WIDTH + TILE_SIZE))
         self.y_pos = BORDER_WIDTH + (column * (BORDER_WIDTH + TILE_SIZE))
         self.surface = pygame.Surface((TILE_SIZE, TILE_SIZE))
@@ -65,7 +55,7 @@ class Tile(pygame.sprite.Sprite):
 
     def change_text(self, value):
         text_colour = CELL_STYLES[value]["font"]
-        text_surface = FONT.render(str(value), True, text_colour, None)
+        text_surface = self.font.render(str(value), True, text_colour, None)
         text_rectangle = text_surface.get_rect(center=(TILE_SIZE/2, TILE_SIZE/2))
         self.surface.blit(text_surface, text_rectangle)
 
@@ -73,35 +63,60 @@ class Tile(pygame.sprite.Sprite):
         fill_colour = CELL_STYLES[value]["fill"]
         self.surface.fill(fill_colour)
 
-all_tiles = pygame.sprite.Group()
+class Game:
 
-def initialise_tiles():
+    def __init__(self):
+        # Set up group to hold tile sprite objects
+        self.all_tiles = pygame.sprite.Group()
+        # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen.fill(BACKGROUND)
+        # Initial the 16 tiles as sprites
+        self.tiles = self.initialise_tiles()
+        self.draw_tiles()
 
-    tiles = []
+    def initialise_tiles(self):
+        tiles = []
+        for row in range(0, NUMBER_OF_ROWS):
+            row_of_tiles = []
+            for column in range(0, NUMBER_OF_COLUMNS):
+                tile = Tile(row, column)
+                row_of_tiles.append(tile)
+                self.all_tiles.add(tile)
+            tiles.append(row_of_tiles)
+        return tiles
 
-    for row in range(0, NUMBER_OF_ROWS):
-        row_of_tiles = []
-        for column in range(0, NUMBER_OF_COLUMNS):
-            tile = Tile(row, column, random.randint(0, 4))
-            row_of_tiles.append(tile)
-            all_tiles.add(tile)
-        tiles.append(row_of_tiles)
-    return tiles
+    def update_tiles(self, tile_values):
+        for row in range(0, NUMBER_OF_ROWS):
+            for column in range(0, NUMBER_OF_COLUMNS):
+                self.tiles[row][column].update(tile_values[row][column])
 
-def draw_tiles(screen):
-    for tile in all_tiles:
-        screen.blit(tile.surface, (tile.x_pos, tile.y_pos))
+    def draw_tiles(self):
+        for tile in self.all_tiles:
+            self.screen.blit(tile.surface, (tile.x_pos, tile.y_pos))
 
-def draw_tiles_old(screen, tiles):
-    for row in range(0, NUMBER_OF_ROWS):
-        for column in range(0, NUMBER_OF_COLUMNS):
-            tile = tiles[row][column]
-            screen.blit(tile.surface, (tile.x_pos, tile.y_pos))
+    @staticmethod
+    def generate_random_tile_values():
+        return [
+            [
+                random.randint(0, 4) for column in range(0, NUMBER_OF_COLUMNS)
+            ] 
+            for row in range(0, NUMBER_OF_ROWS)
+        ]
+        # tile_values = []
+        # for row in range(0, NUMBER_OF_ROWS):
+        #     row_of_tiles = []
+        #     for column in range(0, NUMBER_OF_COLUMNS):
+        #         row_of_tiles.append(random.randint(0, 4))
+        #     tile_values.append(row_of_tiles)
+        # return tile_values
 
-tiles = initialise_tiles()
-draw_tiles(screen)
+# Initialize pygame
+pygame.init()
+game = Game()
 
-pygame.display.flip()
+# Variable to keep the main loop running
+running = True
 
 # Main loop
 while running:
@@ -112,8 +127,15 @@ while running:
             # Was it the Escape key? If so, stop the loop.
             if event.key == K_ESCAPE:
                 running = False
-
+            else:
+                tile_values = Game.generate_random_tile_values()
+                game.update_tiles(tile_values)
+                game.draw_tiles()
 
         # Did the user click the window close button? If so, stop the loop.
         elif event.type == QUIT:
             running = False
+
+    pygame.display.flip()
+
+
